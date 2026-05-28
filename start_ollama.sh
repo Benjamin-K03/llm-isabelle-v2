407be7837dd2b2ce0c2cd1ad1c9154c84089bb9b
Ben K. <benjamin.kingsmill@griffithuni.edu.au>
2026-05-28 23:27:34 +1000

    test: change default debug verbosity

--- start_ollama.sh
+++ start_ollama.sh
@@ -1,4 +1,4 @@
 #!
 -v
 sudo systemctl stop ollama
-sudo OLLAMA_MODELS=/usr/share/ollama/.ollama/models OLLAMA_DEBUG=2 ollama serve
+sudo OLLAMA_MODELS=/usr/share/ollama/.ollama/models OLLAMA_DEBUG=0 ollama serve
